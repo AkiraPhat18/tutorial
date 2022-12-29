210ceeb3ebe11f1d0e09e0ef51a0e949a6dcb4cd
AkiraPhat18 <phatnguyen19111997@gmail.com>
2022-12-29 15:19:22 +0700

    new file

---
+++ javascript.basic/js/index.js
@@ -0,0 +1,34 @@
+
+// let name = 'Akira';
+// console.log(name);
+
+// //Cannot be a reserved keyword
+// //Should be meaningful
+// //Cannot start with a number(1 name)
+// //Cannot contain a space or hyphen (-)
+// //Are  case-sensitive
+
+// let firstName = 'Akira';
+// let lastName = 'Phat';
+
+//const
+// let interestRate  = 0.3;
+// interestRate = 1;
+// console.log(interestRate);
+
+// //This is my first Javascript code
+// console.log('Hello Word');
+
+//object
+let person  = {
+    name: 'Phát',
+    age: 25
+};
+
+//Dot Notation
+person.name = 'John';
+
+//Bracket Notation
+person['name'] = 'Marry'
+
+console.log(person);
